6e670cab995ba34312f6b44d0e4b3b5e685f997f
Nicole <nicole@scratch.com>
2014-06-18 11:33:39 -0400

    Merged master try to fix conflict

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,13 @@
 class StaticPagesController < ApplicationController
-<<<<<<< HEAD
+
 def home
 end
+
 def help
 end
+
 def about
 end
+
 def contact
 end
-end
-=======
-  def home
-  end
-
-  def help
-  end
-
-  def about
-  end
-end
->>>>>>> static-pages
